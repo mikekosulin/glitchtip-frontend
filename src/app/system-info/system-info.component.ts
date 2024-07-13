8599f77bfd67219cd106aa7d984f2238b839a108
Chris Burgess <chris@giantrobot.co.nz>
2024-07-13 17:18:10 +1200

    glitchtip-frontend#306: Wrap in initialLoad$

--- src/app/system-info/system-info.component.ts
+++ src/app/system-info/system-info.component.ts
@@ -32,6 +32,7 @@ import { EntryDataComponent } from "../shared/entry-data/entry-data.component";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SystemInfoComponent {
+  initialLoad$ = this.settingsService.initialLoad$;
   backendConfiguration$ = combineLatest([
     this.settingsService.enableOrganizationCreation$,
     this.settingsService.enableUserRegistration$,
